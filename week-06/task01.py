dc271d5f9b2bc95972661b81df9c2b8ca333b541
Fernando Henrique Ogihara <fernando.ogihara@gmail.com>
2025-11-22 12:29:06 +0000

    file handling

--- week-06/task01.py
+++ week-06/task01.py
@@ -1,1 +1,16 @@
 # WEEK 06
+
+# with open("dataOne.txt", "r") as file:
+#     content = file.read()
+    # print(content)
+
+# create new file with X
+newFile = open("myfile.txt", "x")
+newFile = open("myfile.txt", "w")
+newFile.write("Line 01 Added\n")
+newFile.write("Line 02 Added\n")
+newFile.write("Line 03 Added\n")
+newFile.close()
+
+newFile = open("dataOne.txt", "r")
+print(newFile.read())
