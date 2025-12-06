e08426decb181564d35943e2872e22e5f6e4045e
Fernando Henrique Ogihara <fernando.ogihara@gmail.com>
2025-12-06 12:23:26 +0000

    week 07

---
+++ week-07/task.py
@@ -0,0 +1,19 @@
+# The program should consist of the following two functions:
+
+# The first function should be named observed and should have no parameters.
+def observed()
+# The function should create a set named observations.
+# The function should populate the set with the following items:
+#     "Car", "Sky Scraper", "Sky Scraper", "Bike", "House", "House"
+# Finally, the function should return the set observations.
+
+# The second function should be named run_task1 and should have no parameters.
+# The function should call the first function and display the set returned by the call.
+
+
+# An example run of such a program is shown below:
+
+# {'Car', 'Sky Scraper', 'Bike', 'House'}
+
+
+# Code your solution and be sure to include appropriate comments in your code.
