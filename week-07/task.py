c99ad22c9341be868446cf705cbd00baf3279052
Fernando Henrique Ogihara <fernando.ogihara@gmail.com>
2025-12-06 12:41:29 +0000

    first script and testing

--- week-07/task.py
+++ week-07/task.py
@@ -15,5 +15,36 @@
 
 # {'Car', 'Sky Scraper', 'Bike', 'House'}
 
-
 # Code your solution and be sure to include appropriate comments in your code.
+
+# This program defines two functions to create and display a set of observations.
+
+def observed():
+
+# observations
+  observations = set()
+
+# populate the set
+  observations.add("Car")
+  observations.add("Sky Scraper")
+  observations.add("Sky Scraper")
+  observations.add("Bike")
+  observations.add("House")
+  observations.add("House")
+
+# Return the set
+  return observations
+
+def run_task1():
+
+# Call the observed fn to get the set
+  observation_set = observed()
+
+# Display the set
+  print(observation_set)
+
+# This conditional statement checks if the script is being run directly.
+
+# If it is, it calls the run_task1() fn to start the program.
+if __name__ == "__main__":
+  run_task1()
